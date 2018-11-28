3916e952f9146f7cd12e897eb0eb0190490113fd
Reinaldo <reinaldodipietro@gmail.com>
2018-11-28 00:47:07 -0200

    alalal

---
+++ APS_Programacao/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+    Vector2 direction;
+
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        speed = 10;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+        Vector2 vel = rb.velocity;
+        vel.y = Input.GetAxis("Vertical") * speed;
+
+        rb.velocity = vel;
+    }
+}
---
+++ APS_Programacao/Assets/ScrollingBG.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBG : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(1, 1);
+    public Vector2 direction = new Vector2(-1, 0);
+
+
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+	}
+}
